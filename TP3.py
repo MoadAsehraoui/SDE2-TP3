26f9d6f00c75273d5a5c68e193fbc3353ae1f172
Moad ASEHRAOUI <100540693+MoadAsehraoui@users.noreply.github.com>
2022-04-05 07:56:06 +0100

    Create TP3.py

---
+++ TP3.py
@@ -0,0 +1,50 @@
+#Exo 1 : 
+""""
+var1 = 12
+var2 = 17.9
+var3 = 'SdE2'
+x = type(var1)
+y = type(var2)
+z = type(var3)
+
+print(x , var1)
+print(y , var2)
+print(z , var3) """
+
+#Exo 2
+
+"""nombre = input('Écris un nombre entier positif : ')
+
+nombre = int(nombre)
+
+print('Le programme est en train de vérifier si ce nombre est premier...')
+
+i = 2
+
+while i < nombre and nombre % i != 0 :
+
+    i = i + 1
+
+if i == nombre :
+
+    est_premier = True
+
+else :
+
+    est_premier = False
+
+
+
+if est_premier :
+
+    print('Le nombre', nombre, 'est premier !')
+
+else :
+
+    print('Ce n\'est pas un nombre premier.')""""
+
+    #Exo 3 
+    list = ["Banane", "Orange", "melon","fraise","pomme"]
+    list.sort()
+    print(list)
+
